b0f9c0334940b5af03b0efc2963f1645c337e69b
dotnetdoktor <none>
2020-08-24 12:22:17 +0200

    Ausgangszustand für das Tutorial

---
+++ MiracleListBW/Pages/Index.razor.cs
@@ -0,0 +1,23 @@
+#pragma warning disable 1998
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Web.Pages
+{
+ public partial class Index
+ {
+
+  /// <summary>
+  /// Lebenszyklusereignis: Komponente wird initialisiert
+  /// </summary>
+  /// <returns></returns>
+  protected override async Task OnInitializedAsync()
+  {
+
+  }
+
+ } // end class Index
+}
---
+++ MiracleListBW/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Web
+{
+ public class Program
+ {
+  public static async Task Main(string[] args)
+  {
+   Console.WriteLine("Blazor WebAssembly Main()");
+   Console.WriteLine(System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription);
+   Console.WriteLine("App-Version: " + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version);
+
+   var builder = WebAssemblyHostBuilder.CreateDefault(args);
+   builder.RootComponents.Add<App>("app");
+   AddServices(builder);
+
+   Console.WriteLine("Environment: " + builder.HostEnvironment.Environment);
+   Console.WriteLine("BaseAddress: " + builder.HostEnvironment.BaseAddress);
+   Console.WriteLine("RootComponent: " + builder.RootComponents[0].Selector);
+
+   Console.WriteLine("Starting...");
+   await builder.Build().RunAsync();
+  }
+
+  public static void AddServices(WebAssemblyHostBuilder builder)
+  {
+   IServiceCollection services = builder.Services;
+   services.AddSingleton(new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+  }
+ }
+}
