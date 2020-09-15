8841958784bc18a8f86218e18dafde46da5c492b
dotnetdoktor <none>
2020-09-15 16:44:14 +0200

    zeige erste Kategorie an, wenn es Kategorien gibt!

--- MiracleListBW/Pages/Index.razor.cs
+++ MiracleListBW/Pages/Index.razor.cs
@@ -44,10 +44,11 @@ namespace Web.Pages
    await ShowCategorySet();
   }
 
-
   public async Task ShowCategorySet()
   {
    categorySet = await proxy.CategorySetAsync(am.Token);
+   // zeige erste Kategorie an, wenn es Kategorien gibt!
+   if (this.categorySet.Count > 0) await ShowTaskSet(this.categorySet[0]);
   }
 
    public async Task ShowTaskSet(BO.Category c)
