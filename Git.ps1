3d374ada681fcd0331c00cf4140255355ff8d711
Dr. Holger Schwichtenberg <hs@IT-Visions.de>
2021-08-16 18:59:41 +0200

    Update .NET 6 Preview 7

---
+++ Git.ps1
@@ -0,0 +1,9 @@
+﻿cd H:\git\Github\HeiseBlazorTutorial
+
+
+#git cherry-pick --abort
+
+git checkout Master
+git cherry-pick fc2e9909d26f28f6176ab3f93442d4fe96a28d67
+git commit -m "Update Jahreszahl"
+git push
