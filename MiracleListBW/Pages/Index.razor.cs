c6a1402386733bf31c6a0819c5895406c5f5c50e
Dr. Holger Schwichtenberg <hs@IT-Visions.de>
2021-09-10 14:50:01 +0200

    f

--- MiracleListBW/Pages/Index.razor.cs
+++ MiracleListBW/Pages/Index.razor.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-
 namespace Web.Pages
 {
  public partial class Index
--- MiracleListBW/Program.cs
+++ MiracleListBW/Program.cs
@@ -2,39 +2,39 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 
-namespace Web
+namespace Web;
+
+public class Program
 {
- public class Program
+ public static async Task Main(string[] args)
  {
-  public static async Task Main(string[] args)
-  {
-   Console.WriteLine("Blazor WebAssembly Main()");
-   Console.WriteLine(System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription);
-   Console.WriteLine("App-Version: " + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version);
+  Console.WriteLine("Blazor WebAssembly Main()");
+  Console.WriteLine(System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription);
+  Console.WriteLine("App-Version: " + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version);
 
-   var builder = WebAssemblyHostBuilder.CreateDefault(args);
-   builder.RootComponents.Add<App>("app");
-   AddServices(builder);
+  var builder = WebAssemblyHostBuilder.CreateDefault(args);
+  builder.RootComponents.Add<App>("app");
+  AddServices(builder);
 
-   Console.WriteLine("Environment: " + builder.HostEnvironment.Environment);
-   Console.WriteLine("BaseAddress: " + builder.HostEnvironment.BaseAddress);
-   Console.WriteLine("RootComponent: " + builder.RootComponents[0].Selector);
+  Console.WriteLine("Environment: " + builder.HostEnvironment.Environment);
+  Console.WriteLine("BaseAddress: " + builder.HostEnvironment.BaseAddress);
+  Console.WriteLine("RootComponent: " + builder.RootComponents[0].Selector);
 
-   Console.WriteLine("Starting...");
-   await builder.Build().RunAsync();
-  }
+  Console.WriteLine("Starting...");
+  await builder.Build().RunAsync();
+ }
 
-  public static void AddServices(WebAssemblyHostBuilder builder)
-  {
-   IServiceCollection services = builder.Services;
-   services.AddSingleton(new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+ public static void AddServices(WebAssemblyHostBuilder builder)
+ {
+  IServiceCollection services = builder.Services;
+  services.AddSingleton(new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-   services.AddScoped(typeof(MiracleListAPI.MiracleListProxy));
+  services.AddScoped(typeof(MiracleListAPI.MiracleListProxy));
 
-  }
  }
 }
