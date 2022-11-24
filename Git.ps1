d3c4a60b9fb5651787ebeae777d3ff65674235f9
dotnetdoktor <HSchwichtenberg@users.noreply.github.com>
2022-11-24 17:09:15 +0100

    Update .NET 7.0

--- Git.ps1
+++
@@ -1,9 +0,0 @@
-﻿cd H:\git\Github\HeiseBlazorTutorial
-
-
-#git cherry-pick --abort
-
-git checkout Master
-git cherry-pick fc2e9909d26f28f6176ab3f93442d4fe96a28d67
-git commit -m "Update Jahreszahl"
-git push
